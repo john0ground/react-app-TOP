76b8525258054d4c10c6ab48d9930053e5130d61
John <mjohncorbet@gmail.com>
2024-02-16 19:47:52 +0800

    rendering list of components

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Animals from './Animals.jsx';
 import { PhotographerImg, DogImg } from './Images.jsx';
+import AppAnimals from './ComponentListRender.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Animals />
+        <AppAnimals />
     </React.StrictMode>,
 );
