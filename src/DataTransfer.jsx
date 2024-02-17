50931a5d11e3c03eca416d379a37cbd3c6aa4504
John <mjohncorbet@gmail.com>
2024-02-17 19:23:08 +0800

    set defaultProps on component

--- src/DataTransfer.jsx
+++ src/DataTransfer.jsx
@@ -9,12 +9,18 @@ function Button({ text, color, fontSize }) {
     );
 }
 
+Button.defaultProps = {
+    text: 'Click Me!',
+    color: 'blue',
+    fontSize: 12
+}
+
 export default function DataTransfer() {
     return (
         <div>
             <Button text="Click Me!" color="blue" fontSize={12} />
-            <Button text="Don't Click Me!" color="red" fontSize={12} />
-            <Button text="Click Me!" color="blue" fontSize={20} />
+            <Button text="Don't Click Me!" color="red" />
+            <Button fontSize={20} />
         </div>
     );
 }
