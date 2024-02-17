fc36b1c0d483254d21ee92d914fc8d240345c683
John <mjohncorbet@gmail.com>
2024-02-17 19:16:13 +0800

    destructure props

--- src/DataTransfer.jsx
+++ src/DataTransfer.jsx
@@ -1,11 +1,11 @@
-function Button(props) {
+function Button({ text, color, fontSize }) {
     const buttonStyle = {
-        color: props.color,
-        fontSize: props.fontSize + 'px'
+        color: color,
+        fontSize: fontSize + 'px'
     }
 
     return (
-        <button style={buttonStyle}>{props.text}</button>
+        <button style={buttonStyle}>{text}</button>
     );
 }
 
