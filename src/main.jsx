ff8eb49ff78c15e842ae3a206c89ebed25c85486
John <mjohncorbet@gmail.com>
2024-02-17 19:12:29 +0800

    using props

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import Animals from './Animals.jsx';
-import { PhotographerImg, DogImg } from './Images.jsx';
-import AppAnimals from './ConditionalRender.jsx';
-import TodoList from './KeysToDos.jsx';
+import DataTransfer from './DataTransfer';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <TodoList />
+        <DataTransfer />
     </React.StrictMode>,
 );
